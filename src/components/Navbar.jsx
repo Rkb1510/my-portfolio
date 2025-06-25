92e3168e0d1bbcf8b978f0985b3c9130b2b7d6ed
Rushi Bhuva <rbhuva1996@gmail.com>
2025-06-24 21:07:18 -0600

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import React, {useState} from "react";
 import Scrollspy from "react-scrollspy";
 import { FaHome, FaUser, FaProjectDiagram, FaEnvelope, FaBars, FaTimes } from "react-icons/fa";
 import "./Navbar.css";
+import { ul } from "framer-motion/client";
 
 function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -13,18 +14,18 @@ function Navbar() {
         <nav className="navbar">
             <h1 className="logo">Rushi's Portfolio</h1>
 
-            <div className={`nav-links ${menuOpen ? "open" : ""}`}>
-                <Scrollspy
+            <Scrollspy                
                 items={["home", "about", "projects", "contact"]}
                 currentClassName="is-current"
                 offset={-100}
+                className={`nav-links ${menuOpen ? "open" : ""}`}
             >
                 <li><a href="#home" onClick={() => setMenuOpen(false)}><FaHome className="nav-icon" /> Home</a></li>
                 <li><a href="#about" onClick={() => setMenuOpen(false)}><FaUser className="nav-icon" /> About</a></li>
                 <li><a href="#projects" onClick={() => setMenuOpen(false)}><FaProjectDiagram className="nav-icon" /> Projects</a></li>
                 <li><a href="#contact" onClick={() => setMenuOpen(false)}><FaEnvelope className="nav-icon" /> Contact</a></li>        
             </Scrollspy>
-            </div>
+            
 
             <button className="menu-toggle" onClick={toggleMenu} aria-label="Toggle Menu">
                 {menuOpen ? <FaTimes /> : <FaBars />}
